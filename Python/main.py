48f1bf65023730842a71e3e62107ffe1ed7218cf
tech189 <dlloyd189@gmail.com>
2018-01-18 22:15:21 +0000

    (~) Converted to python 3.x

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.request
 
 url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
-html_soup = BeautifulSoup(urllib2.urlopen(url).read(),'html.parser')
+html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
 texts = html_soup.find('div', attrs={'class': 'text_container'}).get_text()
 
-print texts
+print(texts)
