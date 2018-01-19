a5bc4b14c60cc6c023ea210494ad6d3d87a50e2b
Jake Drew <jakedrew@outlook.com>
2018-01-19 11:51:22 +0000

    (+) Removes line numbers from text

--- Python/main.py
+++ Python/main.py
@@ -4,5 +4,5 @@ import urllib.request
 url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
 html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
 texts = html_soup.find('div', attrs={'class': 'text_container'}).get_text()
-
-print(texts)
+result = ''.join([i for i in texts if not i.isdigit()])
+print(result)
