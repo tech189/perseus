ce5c0c8c2297759ec76cf4b0db0a518ed78d1d00
tech189 <dlloyd189@gmail.com>
2018-01-20 10:12:55 +0000

    (~) Broke but I found the script tags we need

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,8 @@
+# This Python file uses the following encoding: utf-8
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+from selenium import webdriver
 url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
 eng_url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0134%3Abook%3D1%3Acard%3D1"
 html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
@@ -13,3 +15,16 @@ for character in r:
 
 result = ''.join([i for i in texts if not i.isdigit()])
 print(result)
+
+# Prints all the script tags:
+#for a in html_soup.find_all('script'):
+    #print("Found the script tag:", a)
+
+
+# Prints the first part of the array
+print(html_soup.find_all("script")[0])
+
+# Doesn't work why?!??!!?
+for a in html_soup.find_all("script"):
+    if a.re.match("(addDocument)"):
+        print(a)
