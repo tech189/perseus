520f9650cb557abc0addfa3e60d78a16c307e99d
Jake Drew <jakedrew@outlook.com>
2018-01-22 16:54:55 +0000

    (+) Solving the RegEx

--- Python/main.py
+++ Python/main.py
@@ -16,7 +16,7 @@ def printText(url):
     r = "[]%$£@±^&*+=\{\}"
 
     for character in r:
-         texts = texts.replace(character,"")
+        texts = texts.replace(character,"")
 
     result = ''.join([i for i in texts if not i.isdigit()])
 
@@ -25,14 +25,22 @@ def printText(url):
 def printRelatedBooks(url):
     books = {}
     for script in html_soup.find_all("script"):
-        
+
         if re.search("(addDocument\((.+)Perseus(.+))", str(script)):
             regex = re.compile('(?<=addDocument\(\')(.*)(?=\'\);)')
             #books.append(regex.findall(str(script))[0])
             other_book_url = "http://www.perseus.tufts.edu/hopper/text?doc=" + regex.findall(str(script))[0]
             other_book_soup = BeautifulSoup(urllib.request.urlopen(other_book_url).read(),"html.parser")
             title = other_book_soup.find("div", attrs={"id": "header_text"}).get_text().replace("\n", "").replace("&apos;", "'")
+
+            rek = re.compile('(?<=\w)(\\t)(?=\w)')
+            title = rek.sub(' ', title)
+
+            zcy = re.compile('(\\t)')
+            title = zcy.sub('', title)
+
             books[title] = other_book_url
+
     print(books)
 
 
