43740d729e74b58583bf3da4d9a745c66bc3fce2
tech189 <dlloyd189@gmail.com>
2018-01-22 16:57:11 +0000

    (~) Rogue space

--- Python/main.py
+++ Python/main.py
@@ -16,7 +16,7 @@ def printText(url):
     r = "[]%$£@±^&*+=\{\}"
 
     for character in r:
-         texts = texts.replace(character,"")
+        texts = texts.replace(character,"")
 
     result = ''.join([i for i in texts if not i.isdigit()])
 
@@ -32,8 +32,6 @@ def printRelatedBooks(url):
             other_book_url = "http://www.perseus.tufts.edu/hopper/text?doc=" + regex.findall(str(script))[0]
             other_book_soup = BeautifulSoup(urllib.request.urlopen(other_book_url).read(),"html.parser")
             title = other_book_soup.find("div", attrs={"id": "header_text"}).get_text().replace("\n", "").replace("&apos;", "'")
-            regex2 = re.compile("\\t")
-            title = 
             books[title] = other_book_url
     print(books)
 
