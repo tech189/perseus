9cccc92c660ddcb7634c6d98c9963dff7d107c1a
tech189 <dlloyd189@gmail.com>
2018-01-22 14:53:41 +0000

    (+) Started web styling and made scraper easier to test with
    (-) Removed php we won't use that
    (~) We use Iliad Book 1 Line 1 Greek text as the demo but you can test with other texts
    (+) Moved html to root for easier access on a website
    (+) Started styling but I need to learn css grid!

--- Python/main.py
+++ Python/main.py
@@ -3,29 +3,39 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
-eng_url = "http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0134%3Abook%3D1%3Acard%3D1"
-html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
-texts = html_soup.find('div', attrs={'class': 'text_container'}).get_text()
-texts = re.sub('\s{2,}',' ', texts)
-r = "[]%$£@±^&*+=\{\}"
+if input("Demo text? (y/n) ").upper() == "Y":
+    url ="http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
+else:
+    url = input("Type in a perseus url: \n")
 
-for character in r:
+html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
+
+def printText(url):
+    texts = html_soup.find('div', attrs={'class': 'text_container'}).get_text()
+    texts = re.sub('\s{2,}',' ', texts)
+    r = "[]%$£@±^&*+=\{\}"
+
+    for character in r:
          texts = texts.replace(character,"")
 
-result = ''.join([i for i in texts if not i.isdigit()])
-#print(result)
+    result = ''.join([i for i in texts if not i.isdigit()])
 
-books = {}
-a = 0
-for script in html_soup.find_all("script"):
-    
-    if re.search("(addDocument\((.+)Perseus(.+))", str(script)):
-        regex = re.compile('(?<=addDocument\(\')(.*)(?=\'\);)')
-        #books.append(regex.findall(str(script))[0])
-        other_book_url = "http://www.perseus.tufts.edu/hopper/text?doc=" + regex.findall(str(script))[0]
-        other_book_soup = BeautifulSoup(urllib.request.urlopen(other_book_url).read(),"html.parser")
-        title = other_book_soup.find("div", attrs={"id": "header_text"}).get_text().replace("\n", "").replace("&apos;", "'")
-        books[title] = other_book_url
-        a += 1
-print(books)
+    print(result)
+
+def printRelatedBooks(url):
+    books = {}
+    for script in html_soup.find_all("script"):
+        
+        if re.search("(addDocument\((.+)Perseus(.+))", str(script)):
+            regex = re.compile('(?<=addDocument\(\')(.*)(?=\'\);)')
+            #books.append(regex.findall(str(script))[0])
+            other_book_url = "http://www.perseus.tufts.edu/hopper/text?doc=" + regex.findall(str(script))[0]
+            other_book_soup = BeautifulSoup(urllib.request.urlopen(other_book_url).read(),"html.parser")
+            title = other_book_soup.find("div", attrs={"id": "header_text"}).get_text().replace("\n", "").replace("&apos;", "'")
+            books[title] = other_book_url
+    print(books)
+
+
+
+printText(url)
+printRelatedBooks(url)
