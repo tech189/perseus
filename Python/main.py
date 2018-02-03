5f3719ea0fc6197f05d708de254efdf732a07ca0
tech189 <dlloyd189@gmail.com>
2018-02-03 10:25:26 +0000

    Reorganised the functions for getting data, should work inserting data to database

--- Python/main.py
+++ Python/main.py
@@ -4,7 +4,11 @@ import urllib.request
 import re, ast, sys
 import mysql.connector
 
-def printText(url):
+text_content = ""
+text_title = ""
+related_texts = ""
+
+def getTextContent(url):
     texts = html_soup.find('div', attrs={'class': 'text_container'}).get_text()
     texts = re.sub('\s{2,}',' ', texts)
     r = "[]%$£@±^&*+=\{\}"
@@ -13,16 +17,16 @@ def printText(url):
         texts = texts.replace(character,"")
 
     result = ''.join([i for i in texts if not i.isdigit()])
-    print(result)
+    text_content = result
 
-    connection = mysql.connector.connect(user="root", password="root", host="localhost", database="perseus")
-    cursor = connection.cursor()
-    #add_text = "INSERT INTO texts (text_content) VALUES (%s)"
-    cursor.execute("INSERT INTO texts (text_content) VALUES ('%s')", result)
-    connection.commit()
-    print("Added to texts table")
+    print("The content of the text: " + result)
 
-def printRelatedBooks(url):
+def getTextTitle(url):
+    title = html_soup.title.string
+
+    print("Title is:" + title)
+
+def getRelatedTexts(url):
     books = {}
     for script in html_soup.find_all("script"):
 
@@ -44,40 +48,59 @@ def printRelatedBooks(url):
 
     bks = ast.literal_eval(str(books))
     i = 0
+    print("\nRelated books:")
     while (i < len(bks.keys())):
         print(list(bks.keys())[i] + " : " + bks[list(bks.keys())[i]])
         i = i + 1
 
+    related_texts = bks
+
+def insertData(text_title, text_content, related_texts):
+    connection = mysql.connector.connect(user="root", password="root", host="localhost", database="perseus")
+    cursor = connection.cursor()
+
+    cursor.execute("INSERT INTO texts (text_link, text_title, text_content, related_texts) VALUES ('%s, %s, %s')" %(url, text_title, text_content, related_texts))
+    connection.commit()
+
+    print("Added to texts table")
+
+    cursor.close()
+    connection.close()
+
 def readDatabase():
     connection = mysql.connector.connect(user="root", password="root", host="localhost", database="perseus")
     cursor = connection.cursor()
-    cursor.execute("SELECT text_id, text_title FROM texts")
+    cursor.execute("SELECT text_id, text_title, text_content FROM texts")
     row = cursor.fetchone()
 
     if row is None:
         print("Empty texts table")
     while row is not None:
-        print(row[0], row[1])
+        print(row[0], row[1], row[2])
         row = cursor.fetchone()
 
     cursor.close()
     connection.close()
 
 
-help_text = "Perseus scraper\nArguments:\n\t-d\tDemo text (Homer's Iliad Book 1 Line 1)\n\t-u\tScrapes a specific url\n\t-t\tTests the database"
+help_text = "Perseus scraper\nArguments:\n\t-d\tScrapes the demo text (Homer's Iliad Book 1 Line 1)\n\t-u\tScrapes a specific url\n\t-t\tTests the database, prints out texts table"
 
 if len(sys.argv) < 2:
     print(help_text)
 elif sys.argv[1] == "-d":
     url ="http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.01.0133"
     html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
-    printText(url)
-   # printRelatedBooks(url)
+    getTextTitle(url)
+    getTextContent(url)
+    getRelatedTexts(url)
+    insertData(text_title, text_content, related_texts)
+
 elif sys.argv[1] == "-u":
     url = sys.argv[2]
     html_soup = BeautifulSoup(urllib.request.urlopen(url).read(),'html.parser')
-    printText(url)
-    printRelatedBooks(url)
+    getTextTitle(url)
+    getTextContent(url)
+    getRelatedTexts(url)
 elif sys.argv[1] == "-t":
     readDatabase()
 else:
